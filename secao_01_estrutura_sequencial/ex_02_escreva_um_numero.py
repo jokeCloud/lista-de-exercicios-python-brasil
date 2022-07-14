539cead02a2ef9ba094a095939e89c5854f24fd7
jokecloud <medalhaemerson@gmail.com>
2022-07-14 13:44:47 -0300

    003

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -11,6 +11,7 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 
 """
 
-def escreva_um_numero():
-    """Escreva aqui em baixo a sua solução"""
 
+def escreva_um_numero():
+    num = int(input('Digite um núemro: '))
+    print(f'O número informado foi {num}.')
